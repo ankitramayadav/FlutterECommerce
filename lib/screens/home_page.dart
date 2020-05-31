77c7b58b8104fda92f70d712c3495dff9740b84c
ANKIT YADAV <ankit.rama.yadav@outlook.com>
2020-05-31 22:24:05 +0530

    upper tab bar generated with two pages

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,19 +1,47 @@
+import 'package:FlutterECommerce/screens/list/my_list.dart';
 import 'package:flutter/material.dart';
-import 'package:FlutterECommerce/screens/my_list.dart';
+import 'package:FlutterECommerce/screens/list/first_page.dart';
 
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin{
+  TabController controller;
+
+  @override
+  void initState() { 
+    super.initState();
+    controller = new TabController(length: 2, vsync: this);
+  }
+
+  @override
+  void dispose(){
+    controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Flutter Ecommerce'),
+        title: Text('Flutter'),
+        bottom: TabBar(
+        controller: controller,
+          tabs: <Widget>[
+            Tab(icon: Icon(Icons.timer),),
+            Tab(icon: Icon(Icons.account_balance_wallet),)
+          ],
+        ),
       ),
-      body: MyList(),
+      body: TabBarView(
+        controller: controller,
+        children: <Widget>[
+          FirstPage(),
+          MyList()
+        ],
+      ),
     );
   }
 }
---
+++ lib/screens/list/first_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class FirstPage extends StatefulWidget {
+  @override
+  _FirstPageState createState() => _FirstPageState();
+}
+
+class _FirstPageState extends State<FirstPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('hieee'),
+      ),
+    );
+  }
+}
