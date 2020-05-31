5ff639f85281c171b6d321d6a3b20eabbff914f0
ANKIT YADAV <ankit.rama.yadav@outlook.com>
2020-05-31 20:27:44 +0530

    Dummy List Generated with Remove Button

--- lib/screens/my_list.dart
+++ lib/screens/my_list.dart
@@ -15,8 +15,45 @@ class _MyListState extends State<MyList> {
         title: Text('Mobile'),
         subtitle: Text('All Types Of mobiles available'),
         leading: Icon(Icons.face),
+        trailing: RaisedButton(
+          child: Text('Remove'),
+          onPressed: () {
+            deletedialog(context).then((value){
+            print('Value is $value');
+            });
+          },
+        ),
       );
      },
     );
   }
+
+  Future<bool> deletedialog(BuildContext context){
+    return showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context){
+        return AlertDialog(
+          title: Text(
+            'Are Yo Sure ?'
+          ),
+          actions: <Widget>[
+            FlatButton(
+              child: Text('Yes'),
+              onPressed: (){
+              Navigator.of(context).pop(true);
+              },
+              ),
+              FlatButton(
+              child: Text('No'),
+              onPressed: (){
+              Navigator.of(context).pop(false);
+              },
+              ),
+          ],
+        );
+      }
+      );
+  }
+
 }
